27f1a7b66a1959b4311ec1a240d1d64c69cd5736
warren-tap3d <120744440+warren-tap3d@users.noreply.github.com>
2023-03-06 14:17:44 -0600

    First test

---
+++ sniffer.py
@@ -0,0 +1,24 @@
+import socket
+import struct
+import textwrap
+
+# Unpack ethernet frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(dest_mac), socket.htons(proto), data[14:]
+
+# Return formatted MAC address
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ':'.join(bytes_str.upper())
+
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+    print('\nEthernet Frame: ')
+    print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+
+main()   
